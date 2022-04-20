16cf362485d6dbce2e0b7aa0de7970456c4e06e2
Lisbeth Diaz <lizbeths.robles@gmail.com>
2022-04-20 13:09:41 -0400

    added word meanings and synonyms structure

---
+++ src/Components/meanings.js
@@ -0,0 +1,63 @@
+import getMean from '../utils/getMean.js'
+
+
+
+const wordBody = document.querySelector(".word-body");
+
+
+
+export default async function meanings(word){
+    let resolve;
+    
+    await getMean(word)
+    .then(res => resolve = res)
+    .catch(err => console.log(err))
+        
+    
+    resolve.map(i =>{
+      
+        let part = document.createElement("h3");
+        wordBody.appendChild(part)
+        part.textContent = i.partOfSpeech
+    
+        i.definitions.map(j=>{
+
+            let def = document.createElement("p")
+            wordBody.appendChild(def)
+            def.textContent = j.definition
+        })
+    
+        if(i.synonyms.length >0){
+            let synonyms = document.createElement("h3")
+            wordBody.appendChild(synonyms)
+            synonyms.textContent = "Synonyms"
+            let ul = document.createElement("ul")
+            wordBody.appendChild(ul)
+
+            i.synonyms.map(s=>{
+                let li = document.createElement("li")
+                ul.appendChild(li)
+                li.textContent =s
+            })
+        }    
+        if(i.antonyms.length >0){
+            let antonyms = document.createElement("h3")
+            wordBody.appendChild(antonyms)
+            antonyms.textContent = "Antonyms"
+            let ul = document.createElement("ul")
+            wordBody.appendChild(ul)
+
+            i.antonyms.map(s=>{
+                let li = document.createElement("li")
+                ul.appendChild(li)
+            })
+
+        }
+
+
+    })    
+
+    
+
+    
+}
--- src/index.js
+++ src/index.js
@@ -3,38 +3,25 @@ import getApi from './utils/getApi.js'
 import getMean from './utils/getMean.js'
 import getAudio from './utils/getAudio.js'
 
-let word = prompt("Write a word") 
+import meanings from './Components/meanings.js'
+
+let screnWord = document.querySelector(".word-container__word");
+let phonetics = document.querySelector(".word-container__phonectics");
+let word = prompt("type a word in English")
+
 getApi(word)
-  .then((res)=>console.log(res[0].word))
+.then(res=>screnWord.textContent = res[0].word)
+
 getApi(word)
-  .then((res)=>console.log(res[0].phonetic))
-getAudio(word)
-  .then((res)=>console.log(res))
-  
-async function nose(){
- let h = await getMean(word)
-  h.forEach(element => {
-   //getting part of speech
-    console.log(`<br>${element.partOfSpeech}</br>`)
-    //getting definitions
-    element.definitions.forEach(i=>{
-      console.log(i.definition)
-    })
-    //getting synonyms
-    element.synonyms.forEach((j)=>{
-      console.log(j)
-    })
-    //getting antonyms
-    element.antonyms.forEach((u)=>{
-      console.log(u)
-    })
-    
-  });
-}
+.then(res => phonetics.textContent = res[0].phonetic)
+
+
+meanings(word)
 
 
 
- nose();
+
+
 
 
 
